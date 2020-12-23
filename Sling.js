9b1a538167babc7f91c800144b0b0ed356fe3d6d
sriman123456 <71541656+sriman123456@users.noreply.github.com>
2020-12-23 18:23:28 +0530

    Add files via upload

---
+++ Sling.js
@@ -0,0 +1,27 @@
+class Sling {
+    constructor(bodyA,pointB) {
+        var options= {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness: 1,
+            angularStiffness: 1,
+            lenght:220
+        };
+        this.pointB=pointB;
+        this.pointX=pointB.x;
+        this.pointY=pointB.y;
+        this.sling = Constraint.create(options);
+        World.add(world,this.this.sling)
+    }
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            strokeWeight(3.5);
+            stroke("#fff");
+            line(pointB.x, pointB.y, pointA.x, pointA.y);
+            pop();
+        }
+    }
+}
